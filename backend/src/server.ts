998ee213a06992ecc7b28dfd0a1b0874087f697b
Carl Bourges <carlbrgs77@gmail.com>
2025-07-01 11:16:50 +0200

    feat: add PDF summarization feature

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
 import express, { Application } from "express";
+import summarizeRouter from "./routes/summarizeRoute";
+
+
 const PORT = 8000;
 const app: Application = express();
+
+app.use("/api", summarizeRouter);
+
+
 app.get("/", function (req, res) {
  res.send("Hello World!");
 });
+
+
 app.listen(PORT, function () {
   console.log(`Server is listening on port ${PORT}!`);
 });
